d5e7b4c55c7c730bdb2ebb1d5f8321a49d05180d
Amr-Dweikat939 <amrodwaikat@gmail.com>
2024-11-17 13:43:31 +0200

    update database configuration

--- src/config/db.ts
+++ src/config/db.ts
@@ -5,6 +5,14 @@ import { Sequelize } from "sequelize";
 require("dotenv").config();
 
 // Initialize Sequelize with MySQL database credentials
-export const sequelize = new Sequelize(String(process.env.DB_URL), {
-  logging: false,
-});
+export const sequelize = new Sequelize(
+  process.env.DB_NAME as string,
+  process.env.DB_USER as string,
+  process.env.DB_PASSWORD as string,
+  {
+    host: process.env.DB_HOST,
+    port: parseInt(process.env.DB_PORT as string),
+    dialect: "mysql",
+    logging: (...msg) => console.log(msg),
+  }
+);
